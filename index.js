06dc6cc5f603f0056af0c76eb6e8ff13b158be0a
Samidh Patel <spatel13456@gmail.com>
2018-12-14 01:52:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 var recipes = {foo: 'bar'};
 
+<<<<<<< HEAD
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
+=======
+function updateObjectWithKeyAndValue(object, key, value){
+>>>>>>> 4dbf0baf3aa3f5416d4411a32692117b8c9955e4
       object[key] = value;
       return object;
 }
 
+<<<<<<< HEAD
 function updateObjectWithKeyAndValue(object, key, value){
       var newObj = Object.assign({}, object);
       newObj[key] = value;
@@ -24,3 +29,7 @@ function destructivelyDeleteFromObjectByKey(object, key) {
 
 // let obj =updateObjectWithKeyAndValue(recipes, 'bar', 'foo');
 // console.log("obj", obj)
+=======
+let obj =updateObjectWithKeyAndValue(recipes, 'bar', 'foo');
+console.log("obj", obj)
+>>>>>>> 4dbf0baf3aa3f5416d4411a32692117b8c9955e4
